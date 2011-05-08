52346e86834da60e91b9325ee9cd42fbe228b79d
SinZ <acer.pc@live.co.uk>
2011-05-08 15:56:07 +1000

    Added MinecraftQuery commands, Added Some misc commands

--- core/main.php
+++ core/main.php
@@ -131,15 +131,6 @@ class command {
 	public function command_debug($user, $channel, $args) {
 		$this->bot->say_message($channel, json_encode(array($user, $channel, $args)));
 	}
-	public function command_rickroll($user, $channel, $args) {
-		if ($this->user->isadmin($args[0])) {
-			$this->bot->say_message($channel, "Dont rickroll my master, BITCH");
-		}
-		else {
-			$this->bot->say_message($args[0], "Never going to Give you up, Never going to Let you down, never going to run android and, desert you!");
-			$this->bot->say_message($channel, $args[0]." Just got rickrolled!");
-		}
-	}
 	public function command_join($user, $channel, $args) {
 		$this->bot->send_message("","JOIN", $args[0]);
 	}
@@ -192,6 +183,71 @@ class command {
 	public function command_reload($user, $channels, $args) {
 		if($this->user->isAdmin($args[0])) {
 			// Figure out how to reload	
+		}
+	}
+}
+class minecraft_commands {
+	private $bot = null;
+	public function plugin_registered($bot) {
+		$this->bot = $bot;
+	}
+	public function command_playerlist($user, $channel, $args) {
+		require_once("/plugins/Minecraft/Minequery.php");
+		if ($args[0] == "") {
+			$url = "MCSteamed.net";
+		}
+		else $url = $args[0];
+		if ($args[1] == "") {
+			$port = 25566;
+		}
+		else $port = $args[1];
+		$array = Minequery::query($url, $port, $timeout = 30);
+		$char = chr(2);
+		$msg = implode($char." || ".$char, $array["playerList"]);
+		$this->bot->say_message($channel, $msg);
+	}
+	public function command_online($user, $channel, $args) {
+		require_once("/plugins/Minecraft/Minequery.php");
+		if ($args[0] == "") {
+			$url = "MCSteamed.net";
+		}
+		else $url = $args[0];
+		if ($args[1] == "") {
+			$port = 25566;
+		}
+		else $port = $args[1];
+		$array = Minequery::query($url, $port, $timeout = 30);
+		$char = chr(3);
+		$onlineplayers = $array['playerCount'];
+		$maxplayers = $array['maxPlayers'];
+		$maxplayers = preg_replace("/Char/", '', $maxplayers);
+		$this->bot->say_message($channel, "There are currently".$char."21 ".$onlineplayers."".$char." of".$char."21 ".$maxplayers."".$char." players on ".$url." right now.");
+	}
+}
+class fun_commands {
+	private $bot = null;
+	public function plugin_registered($bot) {
+		$this->bot = $bot;
+	}
+	public function command_hug($user, $channel, $args) {
+		$ACTION = chr(1);
+		$this->bot->say_message($channel, $ACTION."ACTION Hugs ".$args[0]." ".$ACTION);
+	}
+	public function command_slap($user, $channel, $args) {
+		$ACTION = chr(1);
+		$this->bot->say_message($channel, $ACTION."ACTION slaps ".$args[0]." with a large frozen tuna!".$ACTION);
+	}
+	public function command_rickroll($user, $channel, $args) {
+		$target = strtolower ($args[0]);
+		if ($target == "sinz") {
+			$this->bot->say_message($channel, "Dont rickroll my master, BITCH");
+		}
+		else if ($target == "mcabot") {
+			$this->bot->say_message($channel, "...dont rickroll me pl0x");
+		}
+		else {
+			$this->bot->say_message($args[0], "Never going to Give you up, Never going to Let you down, never going to run android and, desert you!");
+			$this->bot->say_message($channel, $args[0]." Just got rickrolled!");
 		}
 	}
 }
