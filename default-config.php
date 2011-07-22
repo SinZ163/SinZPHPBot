25f5244ed06e9993dff7518a391c039b5912efd7
Luke Strickland <luke@axxim.net>
2011-07-22 10:06:01 -0500

    Oops, accidentally removed default-config

---
+++ default-config.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+* SinZBot Configuration File
+*
+* Please change the name of this file from default-config.php to config.php
+* 
+*/
+$config['plugins'] = array(
+    'plugin1',
+    'plugin2',
+    'plugin3'
+);
+
+$config['network'] = 'irc.network.tld';
+$config['port']    = '6667';
+$config['ssl']     = false; // NOT READY YET
+$config['nick']    = 'SinZBot';
+$config['ident']   = 'SinZBot';
+$config['realname']= 'SinZBot';
+$config['command'] = '!';
+
+$config['channels']= array('#sinzbot');
+$config['admins']  = array('SinZ');
+						
+
+/* Nick Server */
+$config['ns_enabled'] = false;
+$config['ns_nick'] = 'MCABot';
+$config['ns_pass'] = 'qwertyuiop';
+$config['ns_sla'] = false;
+
+/* CTCP */
+$config['ctcp_ping']    = 'default';
+$config['ctcp_version'] = 'SinZBot';
+
+/* Experimental */
+$config['host']    = 'axxim.net';
