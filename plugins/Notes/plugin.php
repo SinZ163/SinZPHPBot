149d45c5c47efbf7ae6e23bad08bf699024777df
SinZ <acer.pc@live.co.uk>
2011-10-25 19:40:18 +1100

    Notes, Huzzah!

--- plugins/Notes/plugin.php
+++ plugins/Notes/plugin.php
@@ -8,13 +8,29 @@ class Notes {
         $this->bot = $bot;
     }
 
-    public function notes_getDB() {
-        $db = json_decode(file_get_contents('./notes.json'));
-        return $db;
+    public function command_read($user, $channel, $args) {
+        $nick = user::explodeIP($user);
+        if ($args[0]){
+            $auser = $args[0];
+        }
+        else $auser = $nick[0];
+        $player = ucwords(strtolower($auser));
+		$this->bot->notice($user,"| Notes about ".$player." |");
+        $file = file("notes/".$player.".txt");
+        foreach ($file as $note) {
+            $this->bot->notice($user,$note);
+        }
+		$this->bot->notice($user,"| End of Notes |");
     }
-    public function readNote() {
+    public function command_note($user, $channel, $args) {
+        $player = $args[0];
+		$writer=user::explodeIP($user);
+		$this->bot->notice($user,"Written note about ".$player.".");
+		$fp=fopen("notes/".ucwords(strtolower($player)).".txt","a");
+		fwrite($fp,"From ".$writer[0].": ".implode(" ",array_splice($args, 1))."\r\n");
+		fclose($fp);
     }
 
 }
 
-?>
+?>	
